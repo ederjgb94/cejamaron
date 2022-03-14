2bed1f26df2e78eb8fb6432bca85e69d5cab75be
ederjgb94 <a2133330155@alumnos.uat.edu.mx>
2022-03-14 00:06:52 -0600

    clientes + salidas

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $paginador = Cliente::orderBy('created_at', 'DESC')->paginate(5);
+        return jsend_success([
+            'paginas' => $paginador->lastPage(),
+            'clientes' => $paginador->items(),
+        ],);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Cliente::create($request->all());
+        return jsend_success();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cliente $cliente)
+    {
+        return jsend_success(
+            ['cliente'=>$cliente],
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cliente $cliente)
+    {
+        $cliente->update($request->all());
+        return jsend_success();
+    }
+
+}
---
+++ database/migrations/2022_03_04_183926_create_salidas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalidasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salidas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sucursal_id')->nullable()->constrained();
+            $table->string('folio');
+            $table->date('fecha_salida');
+            $table->foreignId('usuario_id')->constrained();
+            $table->decimal('total_importe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salidas');
+    }
+}
